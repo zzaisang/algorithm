d0c01254fa43f0bab8666d93b2965f80eb1f522e
SangJae <zzai_sang@naver.com>
2020-06-28 23:32:49 +0900

    해시/위장 추가

---
+++ src/main/java/algorithm/programers/hash/Camouflage.java
@@ -0,0 +1,19 @@
+package main.java.algorithm.programers.hash;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2020-06-28
+ */
+public class Camouflage {
+
+    public static void main(String[] args) {
+
+        String[][] arrayLv2 = {{"crow_mask", "face"},{"blue_sunglasses","face"},{"smoky_makeup","face"}};
+    }
+
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+}
