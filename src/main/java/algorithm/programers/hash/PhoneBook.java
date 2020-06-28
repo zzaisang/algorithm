625c3c39167f047a2bd5de8dd16f6e7bbe17ba74
SangJae <zzai_sang@naver.com>
2020-06-28 23:24:41 +0900

    PhoneBook 추가. Knumber 추가

--- src/main/java/algorithm/programers/hash/PhoneBook.java
+++ src/main/java/algorithm/programers/hash/PhoneBook.java
@@ -1,5 +1,7 @@
 package main.java.algorithm.programers.hash;
 
+import java.util.Arrays;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
@@ -10,8 +12,12 @@ public class PhoneBook {
         boolean solution = solution(a);
         System.out.println(solution);
 
+        final boolean sol2 = sol2(a);
+        System.out.println(sol2);
+
     }
 
+    //테스트8,9 실패 && 효율성 1,2 실패
     public static boolean solution(String[] phone_book) {
 
         final String baseStr = phone_book[0];
@@ -27,14 +33,18 @@ public class PhoneBook {
         return answer;
     }
 
+    //통과
     public static boolean sol2(String[] phone_book){
 
+        Arrays.sort(phone_book);
         boolean answer = true;
 
-
-
-
-
+        for (int i = 0; i < phone_book.length - 1; i ++){
+            if (phone_book[i + 1].contains(phone_book[i])){
+                answer = false;
+                break;
+            }
+        }
 
         return answer;
     }
---
+++ src/main/java/algorithm/programers/sort/Knumber.java
@@ -0,0 +1,15 @@
+package main.java.algorithm.programers.sort;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2020-06-27
+ * K번째 수
+ */
+public class Knumber {
+
+    public static void main(String[] args) {
+
+    }
+
+}
