8aac5853e994dfed5289fb187de62fbbe4521ce3
SangJae <zzai_sang@naver.com>
2020-06-30 23:57:50 +0900

    Camouflage 풀이중.

--- src/main/java/algorithm/programers/hash/Camouflage.java
+++ src/main/java/algorithm/programers/hash/Camouflage.java
@@ -1,5 +1,10 @@
 package main.java.algorithm.programers.hash;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author zzai_sang
  * @version 0.1.0
@@ -14,6 +19,26 @@ public class Camouflage {
 
     public int solution(String[][] clothes) {
         int answer = 0;
+
+        final HashMap<String,String> hashMap = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            for (int i = 0; i < clothe.length-1; i ++){
+                hashMap.put(clothe[i],clothe[i+1]);
+            }
+        }
+
+        final Set<Map.Entry<String, String>> entrySet = hashMap.entrySet();
+        final Iterator<Map.Entry<String, String>> entryIterator = entrySet.iterator();
+        while (entryIterator.hasNext()){
+            final Map.Entry<String, String> next = entryIterator.next();
+            final String value = next.getValue();
+        }
+
+        for (Map.Entry<String, String> stringStringEntry : entrySet) {
+        }
+
+
         return answer;
     }
 }
