50479be1ab693cd0d2fa4a20164d0c0f11203d0c
SangJae <zzai_sang@naver.com>
2021-02-17 00:28:21 +0900

    Leet Code BestTimeToBuyAndSellStock 추가

---
+++ src/main/java/algorithm/leetcode/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,50 @@
+package main.java.algorithm.leetcode.easy;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 2021-02-16
+ */
+public class BestTimeToBuyAndSellStock {
+
+    /**
+     * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+     *
+     * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+     *
+     * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+     * */
+
+    public static void main(String[] args) {
+        final int[] prices = {7,1,5,3,6,4};
+
+        System.out.println(sol1(prices));
+        System.out.println(sol2(prices));
+    }
+
+    public static int sol1(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }
+
+    public static int sol2(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+
+}
