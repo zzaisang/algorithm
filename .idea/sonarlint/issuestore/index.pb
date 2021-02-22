9f10c65392bb1e291a988cbb3c5403bce4670c99
김상재 <zzaisang@lifegoeson.kr>
2021-02-22 22:37:40 +0900

    AddStrings 추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,9 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 g
 7src/main/java/algorithm/programers/hash/Camouflage.java,4/7/47a79764a9fd7d1ab85d387018095a10ecc155a6
+J
+.idea/libraries/lombok.xml,2/e/2e746fc19417b9c0a6caff5342319552b0f18200
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+F
+zzaisang_algorithm.iml,1/9/19c4caf6ec2ffbf39df33bebb6957e497700c041
---
+++ src/main/java/algorithm/leetcode/easy/AddStrings.java
@@ -0,0 +1,33 @@
+package main.java.algorithm.leetcode.easy;
+
+/**
+ * @author zzai_sang
+ * @version 0.1.0
+ * @since 21. 2. 22.
+ */
+public class AddStrings {
+
+    /**
+     * Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+     *
+     * Note:
+     *
+     * The length of both num1 and num2 is < 5100.
+     * Both num1 and num2 contains only digits 0-9.
+     * Both num1 and num2 does not contain any leading zero.
+     * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+     *
+     * */
+
+    public static void main(String[] args) {
+        String a = "a";
+
+        final char c = a.charAt(0);
+
+        char bb = 98;
+        System.out.println(c);
+        System.out.println(bb);
+    }
+
+
+}
