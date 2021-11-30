f3de9a51013065cc4df01feb2e7dabf70e616994
kimsangjae <zzai_sang@naver.com>
2021-12-01 00:54:59 +0900

    카카오 문제 풀이 완료.

--- src/main/java/algorithm/programers/etc/KakaoNewId.java
+++ src/main/java/algorithm/programers/etc/KakaoNewId.java
@@ -1,7 +1,5 @@
 package main.java.algorithm.programers.etc;
 
-import java.util.stream.Stream;
-
 /**
  * @author zzai_sang
  * @version 0.1.0
@@ -10,22 +8,29 @@ import java.util.stream.Stream;
 public class KakaoNewId {
 
     public static void main(String[] args) {
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        String answer = new_id.toLowerCase(); // 1단계
 
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        answer = answer.replaceAll("[.]{2,}", "."); // 3단계
+        answer = answer.replaceAll("^[.]|[.]$", "");    // 4단계
+
+        if (answer.equals("")) {    // 5단계
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {     // 6단계
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
+        }
+
+        if (answer.length() <= 2) {  // 7단계
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+
+        System.out.println(answer);
     }
-
-    public static String sol(String new_id){
-        return new_id;
-    }
-
-    public static String step1(String id){
-        return id.toLowerCase();
-    }
-
-//    public static String step2(String id){
-//        char[] availableNumberArr = {'0','1','2','3','4','5','6','7','8','9'};
-//        char[] availableSCharArr = {'-','_','.'};
-//
-//        //Stream.of(availableNumberArr,availableSCharArr).
-//    }
 
 }
--- src/main/java/algorithm/programers/etc/KakaoStringAndNumberArray.java
+++ src/main/java/algorithm/programers/etc/KakaoStringAndNumberArray.java
@@ -9,11 +9,16 @@ public class KakaoStringAndNumberArray {
 
     public static void main(String[] args) {
 
+        String s = "one4seveneight";
+        System.out.println(solution(s));
     }
 
-    public static int sol(String s){
-
-        return 0;
+    public static int solution(String s) {
+        String[] numberToWords = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < numberToWords.length; i++) {
+            s = s.replaceAll(numberToWords[i], String.valueOf(i));
+        }
+        return Integer.parseInt(s);
     }
 
 }
